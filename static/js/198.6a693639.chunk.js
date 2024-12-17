2ce590806e91fe4af8980e940659044b9e42e82c
Ayman ALKHATIB <aemn22384@gmail.com>
2024-12-17 14:36:08 +0100

    Updates

---
+++ static/js/198.6a693639.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknotifications_sender=self.webpackChunknotifications_sender||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.6a693639.chunk.js.map
