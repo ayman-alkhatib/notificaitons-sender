a7ba76a19e176c77c99c23b9f832ca66c2bafdb6
Ayman ALKHATIB <aemn22384@gmail.com>
2024-12-17 09:28:15 +0100

    create Form component

---
+++ src/components/Form.jsx
@@ -0,0 +1,127 @@
+import { useState } from "react";
+import { supabase } from "../client";
+
+function Form({ username, avatar }) {
+  const [notificationType, setNotificationType] = useState("followed you");
+  const [target, setTarget] = useState("");
+
+  async function clickfn(notificationType, target, message) {
+    let notification = { username, avatar, notificationType };
+
+    if (target) notification.target = target;
+    if (message) notification.message = message;
+
+    console.log(notification);
+    const { error } = await supabase
+      .from("notifications")
+      .insert(notification)
+      .select();
+
+    if (error) console.error(error);
+  }
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        clickfn(notificationType, target);
+        setNotificationType("followed you");
+        setTarget("");
+      }}
+    >
+      <fieldset
+        onChange={(e) => {
+          setNotificationType(e.target.value);
+        }}
+      >
+        <legend>Select a notification type:</legend>
+        <div>
+          <input
+            type="radio"
+            id="follow"
+            name="drone"
+            value="followed you"
+            defaultChecked
+          />
+          <label htmlFor="follow">follow</label>
+        </div>
+
+        <div>
+          <input
+            type="radio"
+            id="joined your group"
+            name="drone"
+            value="has joined the group"
+          />
+          <label htmlFor="joined your group">joined your group</label>
+        </div>
+
+        <div>
+          <input
+            type="radio"
+            id="left the group"
+            name="drone"
+            value="left the group"
+          />
+          <label htmlFor="left the group">left the group</label>
+        </div>
+
+        <div>
+          <input
+            type="radio"
+            id="like the post"
+            name="drone"
+            value="reacted to your recent post"
+          />
+          <label htmlFor="like the post">like the post</label>
+        </div>
+
+        <div>
+          <input
+            type="radio"
+            id="commented on your picture"
+            name="drone"
+            value="comment on the post"
+          />
+          <label htmlFor="commented on your picture">
+            commented on your picture
+          </label>
+        </div>
+
+        <div>
+          <input
+            type="radio"
+            id="sent private message"
+            name="drone"
+            value="sent private message"
+          />
+          <label htmlFor="sent private message">sent private message</label>
+        </div>
+      </fieldset>
+
+      {(notificationType.includes("group") ||
+        notificationType.includes("post")) && (
+        <div>
+          <label htmlFor="target">target</label>
+          <input
+            type="text"
+            id="target"
+            placeholder="target"
+            value={target}
+            onChange={(e) => {
+              setTarget(e.target.value);
+            }}
+          />
+        </div>
+      )}
+      {notificationType === "sent private message" && (
+        <div>
+          <label htmlFor="message">message</label>
+          <textarea id="message" placeholder="message" />
+        </div>
+      )}
+      <button type="submit">send</button>
+    </form>
+  );
+}
+
+export default Form;
